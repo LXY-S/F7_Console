7db1bd2112ee1d0d4bd53e15e48d9fbff602d124
Mars <mars_29@qq.com>
2023-07-09 19:44:08 +0800

    add mars_key_drv

---
+++ Core/mars_drive/mars_led_drv.c
@@ -0,0 +1,146 @@
+//
+// Created by Mars on 2023/7/8.
+//
+
+#include <stdlib.h>
+#include "mars_led_drv.h"
+
+static pLedHandle mLeds = NULL;
+static uint8_t mLedNum = 0;
+
+static void led_blink(pLedHandle pLed, uint32_t time) {
+    static uint32_t time_old = 0;
+    if (pLed->blink.toggle_num && pLed->blink.toggle_time + time_old >= time) {
+        if (pLed->led_toggle)
+            pLed->led_toggle();
+        (pLed->blink.toggle_num)--;
+        time_old = time;
+    }
+}
+
+static void led_breath(pLedHandle pLed, uint32_t time) {
+    static uint32_t time_old = 0;
+
+    if (!pLed->breath.time || !pLed->breath.set_time)
+        return;
+
+    if (pLed->breath.time + time_old >= time) {
+        if (abs((int8_t)pLed->breath.set_level - (int8_t)pLed->breath.level) < pLed->breath.set_time / pLed->breath.time) {
+            pLed->breath.level = pLed->breath.set_level;
+        } else if (pLed->breath.set_level > pLed->breath.level) {
+            pLed->breath.level += pLed->breath.set_time / pLed->breath.time;
+        } else if (pLed->breath.set_level < pLed->breath.level) {
+            pLed->breath.level -= pLed->breath.set_time / pLed->breath.time;
+        } else {
+            pLed->breath.set_time = 0;
+            pLed->breath.time = 0;
+        }
+        if (pLed->led_level_set && pLed->blink.toggle_num % 2)
+            pLed->led_level_set(pLed->breath.level);
+        time_old = time;
+    }
+}
+
+void mars_led_init(void (*init)(void)) {
+    if (init)
+        init();
+}
+
+void mars_led_reset(void) {
+    mLeds = NULL;
+    mLedNum = 0;
+}
+
+void mars_led_register(pLedHandle pLed) {
+    if (pLed == NULL)
+        return;
+
+    if (pLed->breath.min_level > pLed->breath.max_level) {
+        pLed->breath.min_level = 0;
+        pLed->breath.max_level = 100;
+    }
+    if (pLed->breath.level > pLed->breath.max_level)
+        pLed->breath.level = pLed->breath.max_level;
+    if (pLed->breath.level < pLed->breath.min_level)
+        pLed->breath.level = pLed->breath.min_level;
+
+    pLedHandle tmp = mLeds;
+    while (tmp) {
+        tmp = tmp->next;
+    }
+    mLedNum++;
+    tmp = pLed;
+    tmp->id = mLedNum;
+}
+
+void mars_led_run(uint32_t time) {
+    if (mLeds == NULL)
+        return;
+
+    pLedHandle tmp = mLeds;
+    while (tmp) {
+        led_blink(tmp, time);
+        led_breath(tmp, time);
+        tmp = tmp->next;
+    }
+}
+
+void mars_led_blink_start(uint8_t id, BLINK_FREQ freq, uint32_t time) {
+    if (id == 0 || id > mLedNum)
+        return;
+
+    pLedHandle tmp = mLeds;
+    while (tmp->id != id) {
+        tmp = tmp->next;
+    }
+
+    tmp->blink.toggle_time = freq;
+    tmp->blink.toggle_num = time / freq;
+    if (tmp->blink.toggle_num % 2)
+        tmp->blink.toggle_num++;
+}
+
+void mars_led_blink_stop(uint8_t id) {
+    if (id == 0 || id > mLedNum)
+        return;
+
+    pLedHandle tmp = mLeds;
+    while (tmp->id != id) {
+        tmp = tmp->next;
+    }
+
+    tmp->blink.toggle_num = 0;
+}
+
+void mars_led_breath_start(uint8_t id, uint8_t level, uint32_t time) {
+    if (id == 0 || id > mLedNum)
+        return;
+
+    pLedHandle tmp = mLeds;
+    while (tmp->id != id) {
+        tmp = tmp->next;
+    }
+
+    if (level > tmp->breath.max_level)
+        level = tmp->breath.max_level;
+    if (level < tmp->breath.min_level)
+        level = tmp->breath.min_level;
+
+    tmp->breath.set_level = level;
+    tmp->breath.set_time = time;
+    tmp->breath.time = abs((int8_t)tmp->breath.set_level - (int8_t)tmp->breath.level);
+}
+
+void mars_led_breath_stop(uint8_t id) {
+    if (id == 0 || id > mLedNum)
+        return;
+
+    pLedHandle tmp = mLeds;
+    while (tmp->id != id) {
+        tmp = tmp->next;
+    }
+
+    tmp->breath.set_level = tmp->breath.level;
+    tmp->breath.set_time = 0;
+    tmp->breath.time = 0;
+}
---
+++ Core/mars_drive/mars_led_drv.h
@@ -0,0 +1,55 @@
+//
+// Created by Mars on 2023/7/8.
+//
+
+#ifndef MARS_LED_DRV_H
+#define MARS_LED_DRV_H
+
+#include <stdint.h>
+
+typedef enum {
+    BLINK_FREQ_20HZ = 25U,
+    BLINK_FREQ_8HZ = 62U,
+    BLINK_FREQ_2HZ = 250U,
+    BLINK_FREQ_1HZ = 500U,
+    BLINK_FREQ_05HZ = 1000U,
+} BLINK_FREQ;
+
+typedef struct {
+    uint32_t toggle_time;
+    uint32_t toggle_num;
+} BlinkConfig;
+
+typedef struct {
+    uint8_t level;
+    uint8_t set_level;
+    uint8_t max_level;
+    uint8_t min_level;
+    uint32_t time;      // ms
+    uint32_t set_time;  // ms
+} BreathConfig;
+
+struct Led_t {
+    uint8_t id;
+    BlinkConfig blink;
+    BreathConfig breath;
+    void (*led_on)(void);
+    void (*led_off)(void);
+    void (*led_toggle)(void);
+    void (*led_level_set)(uint8_t level);
+    struct Led_t *next;
+};
+
+typedef struct Led_t LedHandle;
+typedef struct Led_t *pLedHandle;
+
+void mars_led_init(void (*init)(void));
+void mars_led_reset(void);
+void mars_led_register(pLedHandle pLed);
+void mars_led_run(uint32_t time);
+void mars_led_blink_start(uint8_t id, BLINK_FREQ freq, uint32_t time);
+void mars_led_blink_stop(uint8_t id);
+void mars_led_breath_start(uint8_t id, uint8_t level, uint32_t time);
+void mars_led_breath_stop(uint8_t id);
+
+#endif //MARS_LED_DRV_H
