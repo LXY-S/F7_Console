45bcdf801fcceedfa5759e010e654fdc2ee92b08
Mars <mars_29@qq.com>
2023-07-16 23:34:58 +0800

    add mars_app

---
+++ Core/mars_appl/mars_app.c
@@ -0,0 +1,18 @@
+//
+// Created by Mars on 2023/7/16.
+//
+
+#include "mars_app.h"
+#include "mars_config.h"
+#include "tim.h"
+
+void mars_app_init(void) {
+	HAL_TIM_Base_Start_IT(&htim7);
+    mars_shell_init(shell_init, shell_output);
+    mars_led_init(led_init);
+}
+
+void mars_app(void) {
+    mars_shell_run();
+    mars_led_run(time_get());
+}
---
+++ Core/mars_appl/mars_app.h
@@ -0,0 +1,13 @@
+//
+// Created by Mars on 2023/7/16.
+//
+
+#ifndef MARS_APP_H
+#define MARS_APP_H
+
+void mars_app_init(void);
+void mars_app(void);
+
+
+
+#endif //MARS_APP_H
