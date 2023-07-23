8c538e729d624a67baeba3a4da2211c98e666b1a
Mars <mars_29@qq.com>
2023-07-24 00:07:09 +0800

    update mars_app

--- Core/mars_appl/mars_app.c
+++ Core/mars_appl/mars_app.c
@@ -9,12 +9,10 @@
 void mars_app_init(void) {
 	HAL_TIM_Base_Start_IT(&htim7);
     mars_shell_init(shell_init, shell_output);
-    mars_led_init(led_init);
     mars_task_init(task_init);
 }
 
 void mars_app_run(void) {
     mars_shell_run();
-    mars_led_run(time_get());
     mars_task();
 }
